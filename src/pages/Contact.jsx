637d3d72c39e05d36431e3b01c46625fb68306c0
Sweaty Hog <Royceatkins93@gmail.com>
2024-06-07 13:15:57 -0400

    ran npm run build

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,10 +7,10 @@ export default function Contact() {
 
 
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
+        <Form.Text className="text-muted bg-primary">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
@@ -18,9 +18,6 @@ export default function Contact() {
       <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label></Form.Label>
         <Form.Control type="name" placeholder="Name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
